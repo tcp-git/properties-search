534890555f5dc2f00f3f792e3349b1d4a067d5be
tcp-git <firefox0x00@gmail.com>
2025-11-25 06:19:30 +0700

    update redme Each part

--- mercilnew/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-
-df = pd.read_csv(r"C:\Users\bokthaiban\Desktop\mercilnew\assets_rows_merged_with_poi.csv")
-d = pd.read_csv(r"C:\Users\bokthaiban\Desktop\mercilnew\poi_results_enhanced.csv")
-# print(df.columns.tolist())
-print(d['university_name'])
